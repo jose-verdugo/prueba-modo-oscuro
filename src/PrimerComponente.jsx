8aa149b3985ec94ea0d619059085a6e26428a231
Jose Antonio Verdugo <joseantonioverdugoardila@gmail.com>
2023-05-29 13:12:09 +0200

    probando otras funcionalidades

--- src/PrimerComponente.jsx
+++ src/PrimerComponente.jsx
@@ -4,10 +4,24 @@ import ThemeContext from './ModoOscuro'
 function PrimerComponente() {
   const { theme, handleTheme } = useContext(ThemeContext)
 
+  const handleColor = () => {
+    if (theme === 'light') {
+      return 'lorem-light'
+    } else {
+      return 'lorem-dark'
+    }
+  }
+
   return (
     <>
       <div className={`Componente ${theme}`}>
         <h2>Primer Componente</h2>
+
+        <p className={handleColor()}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat ea
+          vel doloremque libero ad quasi deserunt, veniam nisi rerum possimus
+          expedita vero aperiam excepturi sunt. Neque facilis ipsa minus illum.
+        </p>
       </div>
     </>
   )
